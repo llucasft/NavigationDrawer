927cd41cdea62386cc81f8cf17bdc93f40e1493f
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-08-30 06:43:31 -0300

    Criação da página sobre e finalização do projeto.

--- app/src/main/java/com/example/navigationdrawer/ui/sobre/SobreFragment.java
+++ app/src/main/java/com/example/navigationdrawer/ui/sobre/SobreFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 
 import com.example.navigationdrawer.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -25,8 +28,31 @@ public class SobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String descricao = "A ATM consultoria tem como missão apoiar organizações" +
+                " que desejam alcançar o sucesso através da excelência em gestão " +
+                "e da busca pela qualidade. ";
+
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0");
+
+        return new AboutPage( getActivity() )
+                .setImage(R.drawable.logo)
+                .setDescription(descricao)
+
+                .addGroup("Entre em contato ")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("https://google.com", "Acesse nosso site")
+
+                .addGroup("Redes sociais")
+                .addFacebook("lucas.f.teodoro.5", "Facebook")
+                .addInstagram("llucasft", "Instagram")
+                .addGitHub("llucasft", "GitHub")
+
+                .create();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
     }
 
 }
